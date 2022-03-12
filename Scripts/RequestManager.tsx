2e42632bf069076e1c8ac982c495ee107948682b
metinarkh <metin74656@gmail.com>
2022-03-12 16:49:32 +0100

    Create RequestManager.tsx

---
+++ Scripts/RequestManager.tsx
@@ -0,0 +1,36 @@
+export class RequestManager {
+    
+    constructor(){ 
+    }
+
+   
+
+
+    public sendRequest(requestType: RequestManager.Type, endpointData: string){
+       
+        if(requestType === RequestManager.Type.GET){
+               return this.doGetRequest(endpointData);
+           }
+           
+    }
+   async doGetRequest(endpointData:string){
+            try {
+                const response = await fetch(
+                    endpointData
+                );
+                const json = await response.json();
+                return json;
+            } catch(error){
+                console.warn(error);
+         }
+    }
+}
+export namespace RequestManager{
+    export enum Type{
+        GET = "GET",
+        POST = "POST",
+        PUT = "PUT",
+        DELETE = "DELETE"
+
+    }
+}
